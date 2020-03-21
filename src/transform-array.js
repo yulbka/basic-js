f8bf875be167c44b99f9745135387566dc3259f1
yulbka <yulbka13@mail.ru>
2020-03-21 17:04:02 +0300

    feat: resolve task 3-6

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,62 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  let transformedArr = [];  
+
+  function discardNext(arr) {
+  let num = arr.indexOf('--discard-next'); 
+    if (num == (arr.length - 1)) {
+      arr.splice(num, 1);
+    } else {
+    arr.splice(num, 2);    
+  }
+}
+  
+  function discardPrev(arr) {
+  let num = arr.indexOf('--discard-prev');   
+    if (num == 0) {
+      arr.splice(num, 1);
+    } else {
+      arr.splice(num - 1, 2);
+    }  
+}
+  
+  function doubleNext(arr) {
+  let num = arr.indexOf('--double-next');  
+    if (num == (arr.length - 1)) {
+      arr.splice(num, 1);
+    } else {
+    arr.splice(num, 1, arr[num + 1]);
+  }
+}
+
+  function doublePrev(arr) {
+  let num = arr.indexOf('--double-prev');  
+    if (num == 0) {
+      arr.splice(num, 1);
+    } else {
+      arr.splice(num, 1, arr[num - 1]);
+    } 
+}
+
+while (arr.includes('--double-prev')) {
+  doublePrev(arr);
+}
+
+while (arr.includes('--discard-next')) {
+  discardNext(arr);
+}
+
+while (arr.includes('--double-next')) {
+  doubleNext(arr);
+}
+
+while (arr.includes('--discard-prev')) {
+  discardPrev(arr);
+}
+  
+transformedArr = arr;
+
+return transformedArr;
+
+}
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+
+  if (!date) return 'Unable to determine the time of year!';
+
+  date.getTime();
+ 
+   month = date.getMonth();
+   if (month >= 0 && month < 2) return 'winter';
+   if (month >= 2 && month < 5) return 'spring';
+   if (month >= 5 && month < 8) return 'summer';
+   if (month >= 8 && month < 11) return 'autumn';
+   if (month == 11) return 'winter';
+  
+
+}
